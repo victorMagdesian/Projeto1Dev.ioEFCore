46199ca1ca40f255debb9c2b4a3c13d1d2daf752
victorMagdesian <vicfemagdesian@gmail.com>
2021-07-14 22:30:41 -0300

    ef migration

---
+++ Migrations/20210715010226_InitialCreate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DEVIO.PrimeiroProjeto.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+        }
+    }
+}
